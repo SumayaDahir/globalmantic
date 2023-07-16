b438ba37b7e8b1cd3b30b897031ec24071f93487
Maya <DahirowSumaia@gmail.com>
2023-07-15 19:58:21 -0500

    added layout.js and styling to the conference folder

---
+++ app/conference/layout.js
@@ -0,0 +1,15 @@
+
+import style from './styles.css';
+
+export default function ConferenceLayout({ children }) {
+    return (
+<>
+<header style={style.header}>
+    <h1>GLOBALMANTICS MANIACALLY TAKING TECH TO THE GLOBE</h1>
+</header>
+<section>{children}</section>
+</>
+   
+    );
+  }
+  
